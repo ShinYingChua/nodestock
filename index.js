c54b10086f7078399eab76216b7f9591a35dc1c1
CSY <shinyingwendy@gmail.com>
2022-08-26 19:52:15 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+const otherstuff = 'Hello, I am feeling guuuud today';
+
+// Set Handlebar routes
+app.get('/', function(req, res) {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log('Server Listening on Port ' + PORT));
